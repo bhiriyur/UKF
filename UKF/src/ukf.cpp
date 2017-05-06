52e7e5d01eeae661f4a7e1d860de73e0d7c1b5ba
Badri Hiriyur <BHiriyur@thorntontomasetti.com>
2017-05-06 18:32:31 -0400

    Sigma points prediction step

--- UKF/src/ukf.cpp
+++ UKF/src/ukf.cpp
@@ -19,7 +19,7 @@ void UKF::Init() {
 * Programming assignment functions:
 *******************************************************************************/
 
-void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
+void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
 
     //set state dimension
     int n_x = 5;
@@ -27,63 +27,52 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
     //set augmented dimension
     int n_aug = 7;
 
-    //Process noise standard deviation longitudinal acceleration in m/s^2
-    double std_a = 0.2;
-
-    //Process noise standard deviation yaw acceleration in rad/s^2
-    double std_yawdd = 0.2;
-
-    //define spreading parameter
-    double lambda = 3 - n_aug;
-
-    //set example state
-    VectorXd x = VectorXd(n_x);
-    x <<   5.7441,
-            1.3800,
-            2.2049,
-            0.5015,
-            0.3528;
-
-    //create example covariance matrix
-    MatrixXd P = MatrixXd(n_x, n_x);
-    P <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
-            -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
-            0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
-            -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
-            -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
-
-    //create augmented mean vector
-    VectorXd x_aug = VectorXd(7);
-
-    //create augmented state covariance
-    MatrixXd P_aug = MatrixXd(7, 7);
-
-    //create sigma point matrix
+    //create example sigma point matrix
     MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+    Xsig_aug <<
+             5.7441,  5.85768,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.63052,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,
+            1.38,  1.34566,  1.52806,     1.38,     1.38,     1.38,     1.38,     1.38,   1.41434,  1.23194,     1.38,     1.38,     1.38,     1.38,     1.38,
+            2.2049,  2.28414,  2.24557,  2.29582,   2.2049,   2.2049,   2.2049,   2.2049,   2.12566,  2.16423,  2.11398,   2.2049,   2.2049,   2.2049,   2.2049,
+            0.5015,  0.44339, 0.631886, 0.516923, 0.595227,   0.5015,   0.5015,   0.5015,   0.55961, 0.371114, 0.486077, 0.407773,   0.5015,   0.5015,   0.5015,
+            0.3528, 0.299973, 0.462123, 0.376339,  0.48417, 0.418721,   0.3528,   0.3528,  0.405627, 0.243477, 0.329261,  0.22143, 0.286879,   0.3528,   0.3528,
+            0,        0,        0,        0,        0,        0,  0.34641,        0,         0,        0,        0,        0,        0, -0.34641,        0,
+            0,        0,        0,        0,        0,        0,        0,  0.34641,         0,        0,        0,        0,        0,        0, -0.34641;
 
+    //create matrix with predicted sigma points as columns
+    MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+
+    double delta_t = 0.1; //time diff in sec
 /*******************************************************************************
  * Student part begin
  ******************************************************************************/
 
-    //create augmented mean state
-    x_aug << x, 0, 0;
+    for (int i = 0; i < 2*n_aug+1; ++i) {
+        //predict sigma points
+        VectorXd Xi = Xsig_aug.col(i);
+        double px = Xi(0);
+        double py = Xi(1);
+        double v = Xi(2);
+        double yaw = Xi(3);
+        double dyawdt = Xi(4);
+        double noise_v = Xi(5);
+        double noise_yaw = Xi(6);
 
-    //create augmented covariance matrix
-    P_aug.topLeftCorner(n_x, n_x) = P;
-    P_aug(n_x+0,n_x+0) = std_a*std_a;
-    P_aug(n_x+1,n_x+1) = std_yawdd*std_yawdd;
+        VectorXd Xpred_i(5);
+        if (std::fabs(dyawdt)<0.0001) {
+            Xpred_i <<  px     + v*cos(yaw)*delta_t + 0.5*delta_t*delta_t*cos(yaw)*noise_v,
+                        py     + v*sin(yaw)*delta_t + 0.5*delta_t*delta_t*sin(yaw)*noise_v,
+                        v      + 0                  + delta_t*noise_v,
+                        yaw    + dyawdt*delta_t     + 0.5*delta_t*delta_t*noise_yaw,
+                        dyawdt + 0                  + delta_t*noise_yaw;
+        } else {
+            Xpred_i <<  px     + v/dyawdt*( sin(yaw+dyawdt*delta_t)-sin(yaw)) + 0.5*delta_t*delta_t*cos(yaw)*noise_v,
+                        py     + v/dyawdt*(-cos(yaw+dyawdt*delta_t)+cos(yaw)) + 0.5*delta_t*delta_t*sin(yaw)*noise_v,
+                        v      + 0                                            + delta_t*noise_v,
+                        yaw    + dyawdt*delta_t                               + 0.5*delta_t*delta_t*noise_yaw,
+                        dyawdt + 0                                            + delta_t*noise_yaw;
 
-    //std::cout << P_aug << std::endl;
-
-    //create square root matrix
-    MatrixXd A = P_aug.llt().matrixL();
-    A *= sqrt(lambda + n_aug);
-
-    //create augmented sigma points
-    Xsig_aug.col(0) = x_aug;
-    for (int i=0; i < n_aug; ++i) {
-        Xsig_aug.col(1+i      ) = x_aug + A.col(i);
-        Xsig_aug.col(1+i+n_aug) = x_aug - A.col(i);
+        }
+        Xsig_pred.col(i) = Xpred_i;
     }
 
 
@@ -92,29 +81,9 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
  ******************************************************************************/
 
     //print result
-    std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
+    std::cout << "Xsig_pred = " << std::endl << Xsig_pred << std::endl;
 
     //write result
-    *Xsig_out = Xsig_aug;
-
-/* expected result:
-   Xsig_aug =
-  5.7441  5.85768   5.7441   5.7441   5.7441   5.7441   5.7441   5.7441  5.63052   5.7441   5.7441   5.7441   5.7441   5.7441   5.7441
-    1.38  1.34566  1.52806     1.38     1.38     1.38     1.38     1.38  1.41434  1.23194     1.38     1.38     1.38     1.38     1.38
-  2.2049  2.28414  2.24557  2.29582   2.2049   2.2049   2.2049   2.2049  2.12566  2.16423  2.11398   2.2049   2.2049   2.2049   2.2049
-  0.5015  0.44339 0.631886 0.516923 0.595227   0.5015   0.5015   0.5015  0.55961 0.371114 0.486077 0.407773   0.5015   0.5015   0.5015
-  0.3528 0.299973 0.462123 0.376339  0.48417 0.418721   0.3528   0.3528 0.405627 0.243477 0.329261  0.22143 0.286879   0.3528   0.3528
-       0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641        0
-       0        0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641
-
-
-    5.7441   5.85768    5.7441    5.7441    5.7441    5.7441    5.7441    5.7441   5.63052    5.7441    5.7441    5.7441    5.7441    5.7441    5.7441
-     1.38   1.34566   1.52806      1.38      1.38      1.38      1.38      1.38   1.41434   1.23194      1.38      1.38     1.38      1.38      1.38
-   2.2049   2.28414   2.24557   2.29582    2.2049    2.2049    2.2049    2.2049   2.12566   2.16423   2.11398    2.2049   2.2049    2.2049    2.2049
-   0.5015   0.44339  0.631886  0.516923  0.595227    0.5015    0.5015    0.5015   0.55961  0.371114  0.486077  0.407773   0.5015    0.5015    0.5015
-   0.3528  0.299973  0.462123  0.376339   0.48417  0.418721    0.3528    0.3528  0.405627  0.243477  0.329261   0.22143 0.286879    0.3528    0.3528
-        0         0         0         0         0         0  0.774597         0         0         0         0         0        0 -0.774597         0
-        0         0         0         0         0         0         0  0.774597         0         0         0         0        0         0 -0.774597
- */
+    *Xsig_out = Xsig_pred;
 
 }
