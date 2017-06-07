afe511f0fb72916d333db7819332a80a343d07b6
Badri Hiriyur <BHiriyur@thorntontomasetti.com>
2017-06-07 16:12:52 -0400

    Updated source files from laptop

--- UKF/src/main.cpp
+++ UKF/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Eigen/Dense"
-#include <vector>
+// #include <vector>
 #include "ukf.h"
 
 using namespace std;
@@ -17,8 +17,26 @@ int main() {
 * Programming assignment calls
 *******************************************************************************/
 
-    MatrixXd Xsig_pred = MatrixXd(15, 5);
+    MatrixXd Xsig;
+    ukf.GenerateSigmaPoints(&Xsig);
+
+    MatrixXd Xaug;
+    ukf.AugmentedSigmaPoints(&Xaug);
+
+    MatrixXd Xsig_pred;
     ukf.SigmaPointPrediction(&Xsig_pred);
+
+    VectorXd x_pred;
+    MatrixXd P_pred;
+    ukf.PredictMeanAndCovariance(&x_pred, &P_pred);
+
+    VectorXd z_meas;
+    MatrixXd S_meas;
+    ukf.PredictRadarMeasurement(&z_meas, &S_meas);
+
+    VectorXd x_new;
+    MatrixXd P_new;
+    ukf.UpdateState(&x_new, &P_new);
 
     return 0;
 }
